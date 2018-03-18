fae240fe539ef10775b61f3cbfd64a411869171d
Christian Page <christian@localdesert.com>
2018-03-18 13:46:03 -0600

    fix timestamps in logs, fix port in docker-compose.yaml

--- 7dtd.sh
+++ 7dtd.sh
@@ -12,7 +12,7 @@ fi
 
 LOGFILE="${DATA_HOME}/log/7dtd-launcher-$(date +%Y%m%d).log"
 
-echo "$(date +%Y%m%d %T) Checking for server updates" >> ${LOGFILE}
+echo $(date -u) "Checking for server updates" >> ${LOGFILE}
 ${STEAM_HOME}/steamcmd.sh \
   +login anonymous \
   +force_install_dir ${DATA_HOME}/server \
@@ -20,12 +20,12 @@ ${STEAM_HOME}/steamcmd.sh \
   +quit >> ${LOGFILE}
 
 if [ ! -f ${DATA_HOME}/serverconfig.xml ]; then
-  echo "$(date +%Y%m%d %T) Installing default serverconfig.xml into ${DATA_HOME}/serverconfig.xml" >> ${LOGFILE}
-  echo "$(date +%Y%m%d %T) Make sure to update the configuration and restart your server." >> ${LOGFILE}
+  echo $(date -u) "Installing default serverconfig.xml into ${DATA_HOME}/serverconfig.xml" >> ${LOGFILE}
+  echo $(date -u) "Make sure to update the configuration and restart your server." >> ${LOGFILE}
   cp ${DATA_HOME}/server/serverconfig.xml ${DATA_HOME}/serverconfig.xml
 fi
 
-echo "$(date +%Y%m%d% %T) Launching 7 Days to Die Server" >> ${LOGFILE}
+echo $(date -u) "Launching 7 Days to Die Server" >> ${LOGFILE}
 export LD_LIBRARY_PATH=${DATA_HOME}/server:$LD_LIBRARY_PATH
 ${DATA_HOME}/server/7DaysToDieServer.x86_64 \
   -configfile=${DATA_HOME}/serverconfig.xml \
